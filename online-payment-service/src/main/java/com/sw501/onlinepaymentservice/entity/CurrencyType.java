6483866738b723908fa082d9f9419ed56127de03
Samuel Withey <samuelwithey@gmail.com>
2020-04-20 13:24:25 +0100

    Override toString() method to return ENUM Labels

--- online-payment-service/src/main/java/com/sw501/onlinepaymentservice/entity/CurrencyType.java
+++ online-payment-service/src/main/java/com/sw501/onlinepaymentservice/entity/CurrencyType.java
@@ -1,5 +1,22 @@
 package com.sw501.onlinepaymentservice.entity;
 
 public enum CurrencyType {
-    GB_POUNDS, US_DOLLARS, EURO;
+    GB_POUNDS("£"), 
+    US_DOLLARS("$"), 
+    EURO("€");
+    
+    public final String label;
+    
+    private CurrencyType(String label) {
+        this.label = label;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    
+    @Override
+    public String toString() {
+        return label;
+    }
 }
