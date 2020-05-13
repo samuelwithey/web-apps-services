41826118c7fb7d07e42114d4b687ffd3c01a4b18
Samuel Withey <samuelwithey@gmail.com>
2020-05-13 17:36:31 +0100

    Implemented AdminPaymentTransactionBean and viewAllPaymentTransactions web page
    - Refactored admin xhtml to include link to viewAllPaymentTransactions web page

---
+++ online-payment-service/src/main/java/com/sw501/onlinepaymentservice/jsf/AdminPaymentTransactionBean.java
@@ -0,0 +1,28 @@
+package com.sw501.onlinepaymentservice.jsf;
+
+import com.sw501.onlinepaymentservice.ejb.Transaction;
+import com.sw501.onlinepaymentservice.entity.Payment;
+import com.sw501.onlinepaymentservice.entity.Request;
+import java.util.List;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class AdminPaymentTransactionBean implements Serializable {
+    
+    
+    @EJB
+    Transaction transaction;
+    
+
+    public AdminPaymentTransactionBean() {
+    }
+    
+    public List<Payment> getAllPayments() {
+        return transaction.viewAllPayments();
+    }
+    
+}
