948432654ce7bf931310dc32d5402d06bca6cdfb
Samuel Withey <samuelwithey@gmail.com>
2020-05-13 17:16:04 +0100

    Refactored getUserAccounts into UserAccountBean
    - Deleted ViewAllUserAccountBean
    - Updated viewAllUserAccounts.xhtml to use correct jsf bean

--- online-payment-service/src/main/java/com/sw501/onlinepaymentservice/jsf/UserAccountBean.java
+++ online-payment-service/src/main/java/com/sw501/onlinepaymentservice/jsf/UserAccountBean.java
@@ -3,7 +3,9 @@ package com.sw501.onlinepaymentservice.jsf;
 import com.sw501.onlinepaymentservice.ejb.Transaction;
 import com.sw501.onlinepaymentservice.ejb.UserService;
 import com.sw501.onlinepaymentservice.entity.CurrencyType;
+import com.sw501.onlinepaymentservice.entity.UserAccount;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -30,6 +32,10 @@ public class UserAccountBean implements Serializable {
     
     public double getBalance() {
         return usrSrv.getUserAccountBalance();
+    }
+    
+    public List<UserAccount> getUserAccounts() {
+        return usrSrv.viewAllUserAccounts();
     }
     
 }
